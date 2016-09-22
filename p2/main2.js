1d934ea84e5cccbf0f7387a31af12d10835fad5b
sara <sara@nanomia.cl>
2016-09-21 22:03:14 -0300

    p2/index2.html

---
+++ p2/main2.js
@@ -0,0 +1,20 @@
+
+function viajeros(nombre, pais, edad) {
+	this.nombre =nombre;
+	this.pais =pais;
+	this.edad = edad;
+}
+
+var Maria =new viajeros("Maria", "Germany", 25);
+var Thom =new viajeros("Thom", "USA", 28);
+var Jack=new viajeros("Luisa", "England", 29);
+
+
+var amigos = [Maria, Thom, Jack];
+
+gringos = function (viajeros) { 
+	var arreglo_gringos = [] {
+	if (pais == "USA") {
+		console.log (valor.nombre);
+	}
+});
